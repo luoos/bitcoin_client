31c0834b9e98fed62b061893f6935292882bd640
Jun Luo <junluo2@illinois.edu>
2020-01-25 21:55:46 -0600

    Complete assignment 1

---
+++ assignment-1/src/main.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate serde_derive;
+
+use bincode;
+use ring::digest;
+use hex;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NameHash {
+    name: String,
+    hash: String,
+}
+
+fn main() {
+    let name = "Jun Luo";
+    let signature = digest::digest(&digest::SHA256, name.as_bytes());
+
+    let hex_string = hex::encode(signature);
+
+    let name_hash = NameHash {name: name.to_string(), hash: hex_string.to_string()};
+    println!("{:?}", name_hash);
+
+    let encoded: Vec<u8> = bincode::serialize(&name_hash).unwrap();
+    println!("{:?}", encoded);
+
+    let decoded: NameHash = bincode::deserialize(&encoded[..]).unwrap();
+    println!("{:?}", decoded);
+}
