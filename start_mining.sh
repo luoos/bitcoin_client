25f86d2134032335740ac60cbea3c340b989fa94
Jun Luo <junluo2@illinois.edu>
2020-04-15 13:45:55 -0400

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -1,5 +1,6 @@
-curl http://127.0.0.1:7000/miner/start?lambda=1100000
+GAP=1400000
+curl http://127.0.0.1:7000/miner/start?lambda=$GAP
 sleep .2
-curl http://127.0.0.1:7001/miner/start?lambda=1100000
+curl http://127.0.0.1:7001/miner/start?lambda=$GAP
 sleep .2
-curl http://127.0.0.1:7002/miner/start?lambda=1100000
+curl http://127.0.0.1:7002/miner/start?lambda=$GAP
