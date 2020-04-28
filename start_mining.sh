6f65e807db4adce1d4df0bdac5fb445419510e96
Jun Luo <junluo2@illinois.edu>
2020-04-28 13:31:34 -0400

    gitignore auto generated script and data

--- start_mining.sh
+++
@@ -1,6 +0,0 @@
-GAP=1400000
-curl http://127.0.0.1:7000/miner/start?lambda=$GAP
-sleep .2
-curl http://127.0.0.1:7001/miner/start?lambda=$GAP
-sleep .2
-curl http://127.0.0.1:7002/miner/start?lambda=$GAP
